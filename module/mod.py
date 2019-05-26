e4d8a0731c8dacd3aa67ad50d8f9d1fa98fedc8a
Jordy Alkema <me@jordyalkema.nl>
2019-05-26 17:53:08 +0200

    Initial setup

---
+++ module/mod.py
@@ -0,0 +1,25 @@
+import random
+
+from client.comm import BaseComm
+from common.frame_enum import FrameType
+from common.frames import FrameCommandId
+
+
+class Module:
+    def __init__(self, comm: BaseComm):
+        self.comm = comm
+        self.comm.listen_for([FrameType.BUTTON_STATE])
+
+    def process(self):
+        while self.comm.has_data():
+            frame = self.comm.get_data()
+
+            if not frame.request:
+                continue
+
+            frame = FrameCommandId()
+            frame.set_data(random.randint())
+            self.comm.send(frame)
+
+    def stop(self):
+        self.comm.stop()
